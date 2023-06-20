90c485985c280117b23df6559701f9de009a81ea
EmmanuelOkari <emmanuelokari@gmail.com>
2023-06-20 16:04:13 +0300

    Update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,25 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign -> checks if the alphabet is lower case
+ * @n: a character argument
+ * Return: returns 1 and 0 depending on the condition
+ */
 
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		printf("+");
-		return 1;
-	}
-	else if (n == 0)
-	{
-		printf("0");
-		return 0;
-	}
-	else
-	{
-		printf("-");
-		return -1;
-	}
-}
-
-int main(void)
+if (n > 0)
 {
-	int number = -5;
-	int result = print_sign(number);
-	printf("\nSign of %d is %d\n", number, result);
-
-	return 0;
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
 }
