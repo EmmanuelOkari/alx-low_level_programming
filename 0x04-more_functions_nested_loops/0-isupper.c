6968f8b132f0106001bfa0e4bc2b74835d4c9044
EmmanuelOkari <emmanuelokari@gmail.com>
2023-06-22 21:32:04 +0300

    Update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _isupper - checks for lower case character
+ * @c:The character to be checked
+ * Return: 1 for uppercase characters or 0 for anything else
+ */
+
 int _isupper(int c)
 
 {
