9d70dba8e2865c35ebd878ed5003f7fbe5c6f15d
EmmanuelOkari <emmanuelokari@gmail.com>
2023-06-22 22:48:24 +0300

    Update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,19 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-	} else
-	{
-		int i;
-
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+		return;
 	}
 
+	/* Print 'n' underscores in a single write */
+	while (n > 0)
+	{
+		int chunk_size = n > 1024 ? 1024 : n;
+		n -= chunk_size;
+
+		/* Call _putchar multiple times for each chunk of underscores */
+		for (; chunk_size > 0; chunk_size--)
+			_putchar('_');
+	}
+
+	_putchar('\n');
 }
